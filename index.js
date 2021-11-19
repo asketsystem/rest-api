5fbfe5dede0cffc2ecfa16944e4f42bb650c2430
asketsystem <asketsystem1@gmail.com>
2021-11-19 22:54:29 +0000

    Second Commit

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 //TODO: requiring express into our system 
 const express = require('express');
+const mongoose = require('mongoose');
+
+//set up our express app
 const app = express();
 
-// we will set up a GET function 
-app.get('/api', (req, res) => res.send('Its working!'));
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ourdata');
+mongoose.Promise = global.Promise;
 
+app.use(express.static('public'));
 
-app.listen(process.env.port || 4000, function(){
-    console.log('now listening for requests');
+app.use(express.json());
+//initialize routes
+app.use('/api',require('./router/api'));
+
+// error handling middleware
+app.use(function(err,req,res,next){
+    //console.log(err);
+    res.status(422).send({error: err.message});
 });
+
+//listen for requests
+app.listen(process.env.port || 4000, function(){
+    console.log('Ready to Go!');
+});
+
